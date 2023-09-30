11b85121a7942d91ac58fd5f000062eac46f0ce7
Kineolyan <kineolyan@protonmail.com>
2023-09-30 21:05:13 +0200

    Make some changes.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,15 @@
 import * as core from '@actions/core'
-import { wait } from './wait'
+import { process } from './process'
+
+async delayAction(duration: number) => {
+    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
+    core.debug(`Waiting ${delay} milliseconds ...`)
+
+    // Log the current timestamp, wait, then log the new timestamp
+    core.debug(new Date().toTimeString())
+    await wait(ms)
+    core.debug(new Date().toTimeString())
+}
 
 /**
  * The main function for the action.
@@ -7,19 +17,12 @@ import { wait } from './wait'
  */
 export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
+    const delay: number = parseInt(core.getInput('delay'), 10)
+    await delayAction(delay)
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
-
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
+    const report = await process();
+    core.setOutput('report', JSON.stringify(report))
+  } catch (error: unknown) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
   }
