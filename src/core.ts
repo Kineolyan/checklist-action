c7dbd4fde551f2bde252ddf584ed188e68363172
Kineolyan <kineolyan@protonmail.com>
2023-10-02 20:34:02 +0200

    Format code.

--- src/core.ts
+++ src/core.ts
@@ -2,21 +2,21 @@ import * as core from '@actions/core'
 import * as github from '@actions/github'
 
 export type Report = Readonly<{
-  hasChanged: boolean,
-  state: Record<string, boolean>,
+  hasChanged: boolean
+  state: Record<string, boolean>
   captures?: Record<string, string>
 }>
 
 export type PrInfo = Readonly<{
-  owner: string,
-  repo: string,
-  prNumber: number,
-  body: string,
+  owner: string
+  repo: string
+  prNumber: number
+  body: string
 }>
 
 type SwitchInfo = Readonly<{
-  id: string,
-  before: boolean,
+  id: string
+  before: boolean
   after: boolean
 }>
 
@@ -28,38 +28,39 @@ const buildOctokit = () => {
 }
 
 export async function getPrInfo(): Promise<PrInfo> {
-  const octokit = buildOctokit();
+  const octokit = buildOctokit()
 
-    const owner = github.context.repo.owner
-    const repo = github.context.repo.repo
-    const prNumber = parseInt(github.context.payload?.number, 10)
-    const { data: pullRequest } = await octokit.rest.pulls.get({
-        owner,
-        repo,
-        pull_number: prNumber,
-        mediaType: {
-          format: 'patch'
-        }
-    })
-    return {
-      owner,
-      repo,
-      prNumber,
-      body: pullRequest.body ?? '',
+  const owner = github.context.repo.owner
+  const repo = github.context.repo.repo
+  const prNumber = parseInt(github.context.payload?.number, 10)
+  const { data: pullRequest } = await octokit.rest.pulls.get({
+    owner,
+    repo,
+    pull_number: prNumber,
+    mediaType: {
+      format: 'patch'
     }
+  })
+  return {
+    owner,
+    repo,
+    prNumber,
+    body: pullRequest.body ?? ''
+  }
 }
 
 const isEnabled = (checkChar: string) => checkChar !== ' '
 
 const findSwitch = (line: string): SwitchInfo | null => {
-  const pattern = /^\s*- \[( |x|X)\](.*?)<!-- ([a-zA-Z0-9\-_]+) state\[( |x|X)\] -->\s*$/
+  const pattern =
+    /^\s*- \[( |x|X)\](.*?)<!-- ([a-zA-Z0-9\-_]+) state\[( |x|X)\] -->\s*$/
   const match = pattern.exec(line)
-  if (match){
-    const [, after,, id, before] = match;
+  if (match) {
+    const [, after, , id, before] = match
     return {
       id: id.trim(),
       before: isEnabled(before),
-      after: isEnabled(after),
+      after: isEnabled(after)
     }
   } else {
     return null
@@ -73,9 +74,9 @@ export function process(prBody: string): Report {
     .filter(found => found !== null)
     .map(v => v!!)
 
-  const hasChanged = switches.some(({before, after}) => before !== after)
+  const hasChanged = switches.some(({ before, after }) => before !== after)
   const state = switches.reduce(
-    (acc, {id, after}) => {
+    (acc, { id, after }) => {
       acc[id] = after
       return acc
     },
@@ -83,14 +84,14 @@ export function process(prBody: string): Report {
   )
   return {
     hasChanged: hasChanged,
-    state,
+    state
   }
 }
 
-const rewritePrLine = (line: string, {after, before}: SwitchInfo) => {
+const rewritePrLine = (line: string, { after, before }: SwitchInfo) => {
   if (before !== after) {
     const i = line.lastIndexOf('state[')
-    const end = line.indexOf(']', i) 
+    const end = line.indexOf(']', i)
     const newState = after ? 'x' : ' '
     return `${line.substring(0, i)}state[${newState}]${line.substring(end + 1)}`
   } else {
@@ -99,7 +100,8 @@ const rewritePrLine = (line: string, {after, before}: SwitchInfo) => {
 }
 
 export function rewritePrBody(content: string): string {
-  return content.split('\n')
+  return content
+    .split('\n')
     .map(line => {
       const switchDetails = findSwitch(line)
       return switchDetails === null ? line : rewritePrLine(line, switchDetails)
@@ -107,14 +109,14 @@ export function rewritePrBody(content: string): string {
     .join('\n')
 }
 
-export async function updatePr({owner, repo, prNumber, body}: PrInfo) {
+export async function updatePr({ owner, repo, prNumber, body }: PrInfo) {
   const newBody = rewritePrBody(body)
   const octokit = buildOctokit()
 
-   await octokit.rest.pulls.update({
-      owner,
-      repo,
-      pull_number: prNumber,
-      body: newBody
+  await octokit.rest.pulls.update({
+    owner,
+    repo,
+    pull_number: prNumber,
+    body: newBody
   })
 }
